0a212765cb13a3b443e49bf12b2967b8846978e6
Zhong Zhang <31999414+ZhongZhang96@users.noreply.github.com>
2019-07-26 16:44:08 +0800

    code

---
+++ data_preprocessing.sh
@@ -0,0 +1,36 @@
+dataDir=D:/AI/Deecamp/GAN/data/my_test
+
+source activate tf1
+
+python process.py \
+    --input_dir ${dataDir}/original \
+    --operation resize \
+    --output_dir ${dataDir}/resized
+
+conda deactivate
+source activate sketch
+
+python test_pretrained.py \
+    --name pretrained \
+    --dataset_mode test_dir \
+    --dataroot ${dataDir}/resized/ \
+    --results_dir ${dataDir}/Sketch/ \
+    --checkpoints_dir Exp/PhotoSketch/Checkpoints/ \
+    --model pix2pix \
+    --which_direction AtoB \
+    --norm batch \
+    --input_nc 3 \
+    --output_nc 1 \
+    --which_model_netG resnet_9blocks \
+    --no_dropout \
+
+conda deactivate
+source activate tf1
+
+python process.py \
+     --input_dir ${dataDir}/Sketch \
+     --b_dir ${dataDir}/resized \
+     --operation combine \
+     --output_dir ${dataDir}/combined
+
+conda deactivate
